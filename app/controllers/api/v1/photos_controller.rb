32c8151aafd6850b1b520025862da2251278a069
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2016-11-19 17:23:22 -0800

    has_many json 6th

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -28,7 +28,9 @@ class Api::V1::PhotosController < ApplicationController
     @first.count = @count
     @first.save
     @photos = Photo.all.order('id asc')
+    respond_to do |format|
     format.json { render json: @photos }
+      end
   end
 
   def delete
