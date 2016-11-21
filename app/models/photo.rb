b72d54150b59683c2de5ddaf3c88498657c4caf0
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2016-11-20 19:26:45 -0800

    has_many json 6th

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,6 @@
 class Photo < ActiveRecord::Base
   after_initialize :default_values
-  has_many :thumbs, dependent: :destroy
-
+  
   private
     def default_values
       self.url ||= "https://d2udkby987yiki.cloudfront.net/"
--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,3 +1,3 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :url, :count, :user_name
+  attributes :id, :user_id, :url, :count, :user_name, :likes
 end
