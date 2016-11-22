7db66404ce6fd0181cb4d0c6abf58414f96f2995
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2016-11-21 20:46:00 -0800

    added serialized constraint for :likes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,10 @@
 class Photo < ActiveRecord::Base
   after_initialize :default_values
 
+  def likes_condition?
+    true if self.likes != NULL
+  end
+
   private
     def default_values
       self.url ||= "https://d2udkby987yiki.cloudfront.net/"
--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,3 +1,4 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :url, :count, :user_name, :likes
+  attributes :id, :user_id, :url, :count, :user_name
+  attributes :likes, if :likes_condition?
 end
