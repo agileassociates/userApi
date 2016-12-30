4ff5509110dffff4b183fb3678ec4b2cbbf6a8d7
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2016-12-30 03:12:36 -0800

    store accessor or likes method caused null so i removed from active record

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,6 @@
 class Photo < ActiveRecord::Base
   after_initialize :default_values
 
-  store_accessor :likes
-
-  def likes
-  end
-
   private
     def default_values
       self.url ||= "https://d2udkby987yiki.cloudfront.net/"
