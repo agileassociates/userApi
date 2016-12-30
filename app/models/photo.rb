fbdf6dc3dc50ab0a9289b4282c3229a34ddaef29
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2016-12-30 01:41:47 -0800

    added store_accessor

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ActiveRecord::Base
   after_initialize :default_values
 
+  store_accessor :likes
+
   private
     def default_values
       self.url ||= "https://d2udkby987yiki.cloudfront.net/"
