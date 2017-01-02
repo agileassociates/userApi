b43275786a6e371a14cd7a7eed4ca0db6038c0de
Johny Babylon <Home@Johnys-MacBook.local>
2017-01-02 02:28:06 -0800

    devise_for :users, :controllers => {:registrations => "users/registrations"}

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_sign_up_params, only: [:create]
+  skip_before_action :verify_authenticity_token
   respond_to :json
 
-  skip_before_action :verify_authenticity_token
 
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
