088416d6b82aefc727134fb5b6cab0b39a15b75f
Johny Babylon <Home@Johnys-MacBook.local>
2017-01-01 21:16:41 -0800

    devise_for :users, :controllers => {:registrations => "registrations"}

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   namespace :api, defaults: { format: :json } do
     # We are going to list our resources here
     scope module: :v1 do
