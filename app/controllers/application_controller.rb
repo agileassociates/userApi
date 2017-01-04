223282221843867f6d4a84bfa6a34b8a1d51609c
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2017-01-04 04:27:28 -0800

    password not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   #protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ %r{application/json} }
-  #include Authenticable
+  include Authenticable
 
 end
