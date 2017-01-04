7e24f8ea4e177ea270f536ff259c9505b897ab3f
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2017-01-04 04:26:26 -0800

    password not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
-
   protect_from_forgery with: :null_session
 
   #protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ %r{application/json} }
-
-
-
-
-  include Authenticable
+  #include Authenticable
 
 end
