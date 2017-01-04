a560669765ce154a7b091b9192afa8729ae88671
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2017-01-04 02:52:39 -0800

    validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   validates :auth_token, uniqueness: true
   before_create :generate_authentication_token!
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
 
   # Include default devise modules. Others available are:
