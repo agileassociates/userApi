a10ec9515e29282d33fd80b87de83594b96ec9ac
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2017-01-04 03:12:49 -0800

    flatten errors array

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,11 +21,11 @@ class Api::V1::UsersController < ApplicationController
     else
       if user.errors.any?
         if user.errors[:email]
-          render json: { errors: user.errors[:email] }
+          render json: { errors: user.errors[:email]*"," }
         else
           render json: { errors: "Invaid Password"}
         end
-      end
+      ende
     end
   end
 
