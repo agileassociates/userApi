69d1909c14cbbc93a89057ad6de371ac6dda2b4d
Johny Babylon <Home@Johnys-MacBook.attlocal.net>
2017-01-04 13:27:13 -0800

    params.require(:user).permit(:email, :password, :password_confirmation)  & took away downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
   #before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+            format: { with: VALID_EMAIL_REGEX }
 
 
   # Include default devise modules. Others available are:
